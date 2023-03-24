1310bf19030588418da9ad5b32ff680a5a40edb4
Neolize <rumaria.work@gmail.com>
2023-03-24 13:30:38 +0300

    Added '__post_init__' and 'compare_lengths' methods to Vector3D class

--- vector.py
+++ vector.py
@@ -1,17 +1,28 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Vector3D:
     x: int
     y: int
-    z: int
+    z: int = field(compare=False)
+    length: float = field(init=False, repr=False, compare=False)
 
     def __post_init__(self):
         self.length = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
 
+    def compare_lengths(self, other_vector: 'Vector3D') -> str:
+        if self.length == other_vector.length:
+            return "Lengths are equal"
+        return "Lengths aren't equal."
+
 
 if __name__ == "__main__":
-    vector = Vector3D(1, 2, 3)
-    print(vector, vector.length)
-    print(vector.__dict__)
+    first_vector = Vector3D(3, 6, 5)
+    second_vector = Vector3D(3, 6, 2)
+
+    print(f"{first_vector}, length={first_vector.length}")
+    print(f"{second_vector}, length={second_vector.length}")
+
+    print(first_vector == second_vector)
+    print(first_vector.compare_lengths(second_vector))
