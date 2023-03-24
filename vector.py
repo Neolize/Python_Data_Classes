c52bc4eb0eed10b450c1e4caa17ffc0cf23b3c9a
Neolize <rumaria.work@gmail.com>
2023-03-24 14:27:15 +0300

    Method '__post_init__' was updated and added new field 'calc_length'. Now length calculates if field 'calc_length' is True

--- vector.py
+++ vector.py
@@ -1,15 +1,17 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, InitVar
 
 
-@dataclass
+@dataclass(order=True)
 class Vector3D:
     x: int
     y: int
     z: int = field(compare=False)
-    length: float = field(init=False, repr=False, compare=False)
+    calc_length: InitVar[bool] = True
+    length: float = field(init=False, repr=False, compare=False, default=0)
 
-    def __post_init__(self):
-        self.length = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+    def __post_init__(self, calc_length: bool):
+        if calc_length:
+            self.length = (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
 
     def compare_lengths(self, other_vector: 'Vector3D') -> str:
         if self.length == other_vector.length:
@@ -20,9 +22,12 @@ class Vector3D:
 if __name__ == "__main__":
     first_vector = Vector3D(3, 6, 5)
     second_vector = Vector3D(3, 6, 2)
+    third_vector = Vector3D(10, 6, 7, calc_length=False)
 
     print(f"{first_vector}, length={first_vector.length}")
     print(f"{second_vector}, length={second_vector.length}")
+    print(f"{third_vector}, length={third_vector.length}")
 
     print(first_vector == second_vector)
+    print(second_vector > third_vector)
     print(first_vector.compare_lengths(second_vector))
