378347820aa9eef5ef7f3f878fd4453bfd81fcf7
Neolize <rumaria.work@gmail.com>
2023-04-01 11:30:54 +0300

    If __name__ == '__main__' was included into numbers.py

--- numbers.py
+++ numbers.py
@@ -1,9 +1,11 @@
 from math import factorial as fact
 
-number = ""
 
-while number.isdigit() is False:
-    number = input("Enter a number to calculate the factorial: ")
+if __name__ == "__main__":
+    number = ""
 
-print(f"Factorial of {number} is {fact(int(number))}")
-print(f"The square of {number} is {int(number) ** 2}")
+    while number.isdigit() is False:
+        number = input("Enter a number to calculate the factorial: ")
+
+    print(f"Factorial of {number} is {fact(int(number))}")
+    print(f"The square of {number} is {int(number) ** 2}")
