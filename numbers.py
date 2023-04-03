b72b2e7b7420d330d558fc7dc133335934614179
Neolize <rumaria.work@gmail.com>
2023-04-03 16:42:56 +0300

    Deleted a 'print' function

--- numbers.py
+++ numbers.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     number = ""
 
     while number.isdigit() is False:
-        number = input("Enter a number to calculate the factorial: ")
+        number = input("Enter a number to calculate a factorial: ")
 
     print(f"Factorial of {number} is {fact(int(number))}")
-    print(f"The square of {number} is {int(number) ** 2}")
