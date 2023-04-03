44f09c96975c0a791e7dfc6b88ecbc1f369c4c4b
Neolize <rumaria.work@gmail.com>
2023-04-03 17:10:06 +0300

    Added a new 'print' function with a sequence of factorials to 'numbers.py'

--- numbers.py
+++ numbers.py
@@ -8,3 +8,5 @@ if __name__ == "__main__":
         number = input("Enter a number to calculate a factorial: ")
 
     print(f"Factorial of {number} is {fact(int(number))}")
+    print(f"A sequence of factorials of the given number: "
+          f"{', '.join(str(fact(item)) for item in range(1, int(number) + 1))}")
